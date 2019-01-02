26d0727d8e945b2b188d221bbe75a0d91d15aacd
David Bagarian <david.bagaryan@mail.ru>
2019-01-02 15:35:07 +0300

    added detail view and some refactoring

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+from .models import Post
+
 
 def posts_list(request):
-    names = ['ole', 'ksu', 'masha', 'ola']
-    return render(request, 'blog/index.html', {'title': 'new title', 'names': names})
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', {'title': 'list posts', 'posts': posts})
+
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog/post_detail.html', {'title': f'post {slug} detail', 'post': post})
